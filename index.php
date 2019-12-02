1f06b67a4f81b82cf2a9c4c9362f9cc260c09ffc
Moreaulouis <moreau.louismichel@gmail.com>
2019-12-02 15:11:53 +0100

    PHP

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php $firstname = 'Louis';
+      $lastname = 'Moreau';
+      $age = 21;
+      ?>
+<!DOCTYPE html>
+<html lang=fr dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Exo 2</title>
+  </head>
+  <body>
+    <p>
+    <?php echo $firstname ?>
+      <?php echo $lastname ?>
+        <?php echo $age ?>
+        <p>
+
+  </body>
+</html>
